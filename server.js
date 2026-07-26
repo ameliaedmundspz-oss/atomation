a4781cd42f5f3bb9cf2331ee0476b9094cdee1da
ameliaedmundspz-oss <ameliaedmundspz@gmail.com>
2026-07-26 19:34:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.use(cors());
 
 // Your URLs (change these)
 const urls = [
-  "https://www.base64-image.de/",
-  "https://chat.deepseek.com/",
-  "https://www.google.com/search?q=how+to+convert+image+into+data+base+base64+Read+12+web+pages+Converting+an+image+into+a+Base64+string+is+a+common+task+for+web+developers%2C+often+used+to+embed+images+directly+into+HTML%2C+CSS%2C+or+JSON.+There+are+several+ways+to+do+this%2C+depending+on+your+preferred+workflow.+%F0%9F%92%BB+How+to+Do+It+in+Code&sourceid=chrome&ie=UTF-8"
+  "https://jellyfish-app-zlskm.ondigitalocean.app/",
+  "https://jellyfish-app-zlskm.ondigitalocean.app/",
+  "https://jellyfish-app-zlskm.ondigitalocean.app/"
 ];
 
 app.get("/get-url", (req, res) => {
